3ad9f6a3f5e870e9eec79663f31598cc3704d6de
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2022-11-05 18:36:22 -0300

    fix Mozo

--- Cerveceria/src/main/java/modelo/Mozo.java
+++ Cerveceria/src/main/java/modelo/Mozo.java
@@ -1,49 +1,22 @@
 package modelo;
 
-import java.time.LocalDate;
-
 //Clase de los mozos
-
 public class Mozo {
-<<<<<<< Updated upstream
     private String nombreYApellido;
     private int edad;
-=======
-    /*
-    Constructor de la clase mozo
-    @param nombre nombre del mozo
-    @param fechaNacimiento fecha en la que nacio el mozo, el mozo siempre tiene 18 anos o mas
-    @param estado estado del mozo durante dia laboral 0 -> activo , 1-> ausente 2-> franco
-     */
     private String nombre;
-    private LocalDate fechaNacimiento; //RESOLVER
->>>>>>> Stashed changes
     private int cantHijos;
     private String estado; // Activo - Ausente - Franco \\ un invariable podria ser que el estado nunca sea != a 0,1,2
 
-<<<<<<< Updated upstream
     //CONSTRUCTOR
     public Mozo(String nombre, int edad, int cantHijos, String estado) throws Exception {
         this.nombreYApellido = nombre;
-=======
-    public Mozo(String nombre, LocalDate fechaNacimiento, int cantHijos, int estado) throws Exception {
-        this.nombre = nombre;
->>>>>>> Stashed changes
         this.estado = estado;
 //        if (!this.esMayorDeEdad(fechaNacimiento)) throw new Exception();
         this.edad = edad;
 //        if (cantHijos < 0) throw new Exception();
         this.cantHijos = cantHijos;
     }
-
-    // este metodo podria ir en utils
-    // @precondion : fechaNacimiento tiene que ser != NULL
-    // retorna true si la edad es mayor o igual a 18 anos
-    //public boolean esMayorDeEdad(Date fecha){
-       // Date start = Date.from( 2010 , 1 , 1 ) ;
-        //long years = java.time.temporal.ChronoUnit.YEARS.between( start , fecha );
-        //return years >= 0 ;
-    //}
 
     public String getNombreYApellido() {
         return nombreYApellido;
