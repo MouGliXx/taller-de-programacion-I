7ae59ebedae90d4bed3d22088b216964dc022bb9
ignaciosuarezquilis <ignaciogustavosuarez1234@gmail.com>
2022-11-14 11:07:50 -0300

    das

--- Cerveceria/src/test/java/cajaBlanca/aplicarPromocionesProductosTest1.java
+++ Cerveceria/src/test/java/cajaBlanca/aplicarPromocionesProductosTest1.java
@@ -32,7 +32,7 @@ public class aplicarPromocionesProductosTest1 {
             Factura factura=this.escenario.cerveceria.getFacturas().get(0);
             System.out.println(factura);
 
-            assertEquals(180.0,factura.getTotal());
+            assertEquals(100.0,factura.getTotal());
 
         }
         catch(Exception e){
